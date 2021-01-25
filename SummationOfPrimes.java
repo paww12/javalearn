60897c19da39153ea04a4afff11b0feff12443a9
paww12 <77700850+paww12@users.noreply.github.com>
2021-01-25 23:06:56 +0100

    Add files via upload

--- SummationOfPrimes.java
+++ SummationOfPrimes.java
@@ -6,13 +6,13 @@ public class SummationOfPrimes {
 		// TODO Auto-generated method stub
 
 		System.out.println(sumOfNPrimes(2000000));
-		
+
 	}
 
 	private static long sumOfNPrimes(int upperLimit) {
 		long sumOfNPrimes = 0;
 
-		for (int i = 3; i < upperLimit; i+=2) {
+		for (int i = 3; i < upperLimit; i += 2) {
 
 			if (isPrime(i)) {
 
@@ -20,12 +20,12 @@ public class SummationOfPrimes {
 			}
 		}
 
-		return sumOfNPrimes+2;
+		return sumOfNPrimes + 2;
 	}
 
 	private static boolean isPrime(int value) {
 
-		for (int i = 2; i*i <= value; i++) {
+		for (int i = 2; i * i <= value; i++) {
 			if (value % i == 0) {
 				return false;
 			}
